a75ef4b7c6bc42f1f21263d9f7a46a23a3337448
DuckFromLake <121292875+DuckFromLake@users.noreply.github.com>
2023-10-15 14:06:35 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace FrameLab7
 {
@@ -12,11 +14,11 @@ namespace FrameLab7
     {
         public static int ShowArray(int[,] array, int a, int b)
         {
-            for (int i = 0; i < a; i++ )
+            for (int i = 0; i < a; i++)
             {
                 for (int j = 0; j < b; j++)
                 {
-                    Console.Write(array[i,j] + " ");
+                    Console.Write(array[i, j] + " ");
                 }
                 Console.WriteLine();
             }
@@ -29,7 +31,7 @@ namespace FrameLab7
             {
                 for (int j = 0; j < b; j++)
                 {
-                    array[i, j] = random.Next(-1,2);
+                    array[i, j] = random.Next(-1, 2);
                 }
             }
             return 0;
@@ -46,9 +48,9 @@ namespace FrameLab7
                 {
                     s += array[i, j];
                 }
-                if (s/b < 0)
+                if (s / b < 0)
                 {
-                    count++;  
+                    count++;
                 }
             }
             return count;
@@ -78,20 +80,60 @@ namespace FrameLab7
             return S;
         }
 
+        public static double FindK(in double u, in double v, out double k)
+        {
+            k = Math.Min(u + (v * 2), 3.14);
+            return 0;
+        }
+
+        public static double FindU(in double a, in double b, out double u)
+        {
+            u = Math.Max(a, b - a);
+            return 0;
+        }
+
+        public static double FindV(in double a, in double b, out double v)
+        {
+            v = Math.Min(a * b, a + b);
+            return 0;
+        }
+
+        public static int NewPoint(ref int x, ref int y)
+        {
+            string s;
+            Console.WriteLine("x точки: ");
+            s = Console.ReadLine();
+            x = Convert.ToInt32(s);
+            Console.WriteLine("y точки: ");
+            s = Console.ReadLine();
+            y = Convert.ToInt32(s);
+            return 0;
+        }
+
+        public static int chetvert(in int x, in int y, out int quad)
+        {
+            quad = 0;
+            if (x > 0 && y > 0) { quad = 1; }
+            if (x < 0 && y > 0) { quad = 2; }
+            if (x < 0 && y < 0) { quad = 3; }
+            if (x > 0 && y < 0) { quad = 4; }
+            return 0;
+        }
+
 
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             string s, s1;
-            int v = -1;
-            while (v != 0)
+            int v2 = -1;
+            while (v2 != 0)
             {
                 Console.Clear();
                 Console.WriteLine("1) Консольное приложение \n2) Windows Forms \n\n0) Выход \n\nВведите номер метода : ");
                 s = Console.ReadLine();
-                v = Convert.ToInt32(s);
-                switch (v)
+                v2 = Convert.ToInt32(s);
+                switch (v2)
                 {
                     case 1:
                         {
@@ -101,7 +143,7 @@ namespace FrameLab7
                             while (v1 != 0)
                             {
                                 Console.Clear();
-                                Console.WriteLine("1) Задание 1.1\n2) Задание 1.2\n3) Задание 1.3 \n\n0) Выход \n\nВведите номер метода : ");
+                                Console.WriteLine("1) Задание 1.1\n2) Задание 1.2\n3) Задание 1.3\n4) Задание 2\n5) Задание 3\n\n0) Выход \n\nВведите номер метода : ");
                                 sn = Console.ReadLine();
                                 v1 = Convert.ToInt32(sn);
                                 switch (v1)
@@ -122,7 +164,7 @@ namespace FrameLab7
 
                                             RandomArray(array, a, b);
                                             Console.WriteLine("Массив: ");
-                                            ShowArray(array, a , b);
+                                            ShowArray(array, a, b);
                                             Console.WriteLine("Количество строк, у которых среднее арифметическое элементов меньше 0: " + BelowZeroArray(array, a, b));
 
                                             Console.WriteLine("Нажмите любую кнопку для продолжения...");
@@ -145,8 +187,8 @@ namespace FrameLab7
                                             s2 = Console.ReadLine();
                                             z = Convert.ToDouble(s2);
 
-                                            Console.WriteLine("a = " + Func1(x,y));
-                                            Console.WriteLine("b = " + Func2(x,z));
+                                            Console.WriteLine("a = " + Func1(x, y));
+                                            Console.WriteLine("b = " + Func2(x, z));
                                             Console.WriteLine("Нажмите любую кнопку для продолжения...");
                                             s1 = Console.ReadLine();
                                             break;
@@ -161,7 +203,7 @@ namespace FrameLab7
                                             Console.WriteLine("Введите размер массива: ");
                                             s1 = Console.ReadLine();
                                             a = Convert.ToInt32(s1);
-                                            array = new int[a]; 
+                                            array = new int[a];
                                             for (int i = 0; i < a; i++)
                                             {
                                                 array[i] = random.Next(10);
@@ -169,14 +211,55 @@ namespace FrameLab7
                                             Console.Write("Массив: ");
                                             for (int i = 0; i < a; i++)
                                             {
-                                               Console.Write(array[i] + " ");
+                                                Console.Write(array[i] + " ");
                                             }
                                             Console.WriteLine();
                                             Console.WriteLine("Cуммв всех элементов равна: " + Task3(array, a));
                                             Console.WriteLine("Нажмите любую кнопку для продолжения...");
                                             s1 = Console.ReadLine();
                                             break;
-
+                                        }
+                                    case 4:
+                                        {
+                                            Console.Clear();
+                                            double a, b, v, u, k;
+                                            Console.WriteLine("Введите a: ");
+                                            s1 = Console.ReadLine();
+                                            a = Convert.ToInt32(s1);
+                                            Console.WriteLine("Введите b: ");
+                                            s1 = Console.ReadLine();
+                                            b = Convert.ToInt32(s1);
+                                            FindU(in a, in b, out u);
+                                            FindV(in a, in b, out v);
+                                            FindK(in u, in v, out k);
+                                            Console.WriteLine("u = " + u);
+                                            Console.WriteLine("v = " + v);
+                                            Console.WriteLine("k = " + k);
+                                            Console.WriteLine("Нажмите любую кнопку для продолжения...");
+                                            s1 = Console.ReadLine();
+                                            break;
+                                        }
+                                    case 5:
+                                        {
+                                            Console.Clear();
+                                            int x1 = 0, y1 = 0, x2 = 0, y2 = 0, quad1, quad2;
+                                            Console.WriteLine("Введите координаты  первой точки (не 0): ");
+                                            NewPoint(ref x1, ref y1);
+                                            Console.WriteLine("Введите координаты  второй точки (не 0): ");
+                                            NewPoint(ref x2, ref y2);
+                                            chetvert(in x1, in y1, out quad1);
+                                            chetvert(in x2, in y2, out quad2);
+                                            if (quad1 == quad2)
+                                            {
+                                                Console.WriteLine("Точки лежат в одной четверти");
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("Точки НЕ лежат в одной четверти");
+                                            }
+                                            Console.WriteLine("Нажмите любую кнопку для продолжения...");
+                                            s1 = Console.ReadLine();
+                                            break;
                                         }
                                     case 0:
                                         {
